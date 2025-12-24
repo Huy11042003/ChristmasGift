ef7f86877b06b3fc999876e53a72ec3a1e2064c5
unknown <coipham1104@gmail.com>
2025-12-24 22:15:22 +0700

    update music

--- constants.ts
+++ constants.ts
@@ -8,7 +8,7 @@ export const GREETINGS: Greeting[] = [
 ];
 
 // Sử dụng URL trực tuyến thay vì file local
-export const AUDIO_URL = "/assets/music.mp3";
+export const AUDIO_URL = "./assets/music.mp3";
 
 export const COLORS = [
   "#ff0000", // Red
